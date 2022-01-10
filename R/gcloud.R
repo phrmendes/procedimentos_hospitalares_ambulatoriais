64d27861c55b2bcd2ad86885dfe1ad2fdbbfc0d4
Pedro Mendes <pedro.mendes@abramge.com.br>
2022-01-10 17:52:19 -0300

    - Nome correto do projeto - Atualização de renv

---
+++ R/gcloud.R
@@ -0,0 +1,8 @@
+# bibliotecas e funções ---------------------------------------------------
+
+source("R/bibliotecas.R")
+source("R/funcoes.R")
+source("R/dic_tuss_db.R")
+
+# autenticação - gcloud ---------------------------------------------------
+
