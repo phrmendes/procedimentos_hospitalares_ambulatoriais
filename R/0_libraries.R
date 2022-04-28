d35c1d7e77fa66902e25fac2ab98659db0d5d1cc
Pedro Mendes <phrmendes@tuta.io>
2022-04-28 16:03:47 -0300

    Atualização de renv.

--- R/0_libraries.R
+++ R/0_libraries.R
@@ -6,10 +6,9 @@ if (!require("pacman")) install.packages("pacman")
 
 # install.packages("arrow", repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest")
 
-pacman::p_load_gh("ipeaGIT/geobr/r-package", install = FALSE)
-
 pacman::p_load(
   data.table,
+  devtools,
   collapse,
   tidyverse,
   glue,
@@ -28,3 +27,6 @@ pacman::p_load(
   lintr,
   install = FALSE
 )
+
+pacman::p_load_gh("ipeaGIT/geobr/r-package", install = FALSE)
+
