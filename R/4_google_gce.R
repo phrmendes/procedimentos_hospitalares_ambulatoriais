ed9aaef3012eb48c3a33c18042d9de3626e6078a
Pedro Mendes <phrmendes@tuta.io>
2022-05-03 10:41:44 -0300

    Alterações na VM.

--- R/4_google_gce.R
+++ R/4_google_gce.R
@@ -1,11 +1,12 @@
-pacman::p_load(googleComputeEngineR, install = TRUE)
+pacman::p_load(googleComputeEngineR, install = FALSE)
 
 vm <- googleComputeEngineR::gce_vm(
   name = "dashboard-vm",
-  predefined_type = "n1-standard-8",
+  predefined_type = "n1-standard-16",
   template = "rstudio",
   username = "phrmendes",
-  password = "01001011"
+  password = "01001011",
+  disk_size_gb = 200
 )
 
 googleComputeEngineR::gce_vm_start("dashboard-vm")
